0d688f796ea181718ace9faa44f123b8355c333a
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-13 12:07:08 -0300

    feat: add get-links route

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import { confirmParticipants } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 
@@ -26,6 +27,7 @@ app.register(confirmParticipants);
 app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running!");
