5311575c7b0b18077ebedcf2ad68bf85e28fafd7
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-12 22:02:42 -0300

    feat: sort imports and new small link

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -1,10 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import nodemailer from "nodemailer";
 import { z } from "zod";
-import { prisma } from "../lib/prisma";
 import { dayjs } from "../lib/dayjs";
 import { getMailClient } from "../lib/mail";
-import nodemailer from "nodemailer";
+import { prisma } from "../lib/prisma";
 
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -52,7 +52,7 @@ export async function confirmTrip(app: FastifyInstance) {
 
       await Promise.all(
         trip.participants.map(async (participant) => {
-          const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm/${participant.id}`;
+          const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`;
 
           const message = await mail.sendMail({
             from: {
