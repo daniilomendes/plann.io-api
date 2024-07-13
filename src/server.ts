fbe67d4a643d06c5130be74d4bf090dc52285ff3
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-13 12:37:51 -0300

    feat: add create-invite  route

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
 
 const app = fastify();
 
@@ -30,6 +31,7 @@ app.register(getActivities);
 app.register(createLink);
 app.register(getLinks);
 app.register(getParticipants);
+app.register(createInvite);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running!");
